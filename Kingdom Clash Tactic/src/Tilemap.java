a7c6d164fa47f05e35291e51ce75e4a5e182061a
AnantaPW <42257927+AnantaPW@users.noreply.github.com>
2019-04-11 10:55:01 +0700

    Update Tilemap.java

--- Kingdom Clash Tactic/src/Tilemap.java
+++ Kingdom Clash Tactic/src/Tilemap.java
@@ -1,69 +1,70 @@
-import java.awt.Color;
-import java.awt.Graphics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Tilemap {
-	private String name;
-	private int tile_size;
-	private int x;
-	private int y;
-	private int [][] map;
-	private int map_width;
-	private int map_height;
-	
-	public Tilemap(String fileName, int tileSize) {
-		this.tile_size = tileSize;
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(fileName));
-			
-			//get mapWidth and mapHeight
-			map_width = Integer.parseInt(br.readLine());
-			map_height = Integer.parseInt(br.readLine());
-			
-			//make map with mapHeight & mapWidth
-			map = new int [map_height][map_width];
-			
-			//Separate every integer by " "
-			String separate = " ";
-			for(int baris = 0; baris < map_height; baris++) {
-				//read every line
-				String line = br.readLine();
-				String[] tokens = line.split(separate);
-				for(int kolom = 0; kolom < map_width; kolom++) {
-					//convert + input every integer
-					map[baris][kolom] = Integer.parseInt(tokens[kolom]);
-				}
-				br.close();
-			}
-		}catch(Exception e) {
-			System.out.println("File not found!");
-		}
-	}
+
+	private int[][] mapCode;
 	
 	public Tilemap() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void draw(Graphics g) {
-		System.out.println("Map width = " + map_width);
-		System.out.println("Map height = " + map_height);
-		for(int i = 0;i < map_height;i++) {
-			for(int j = 0;j < map_width;j++) {
-				int curr = map[i][j];
-				if(i == 0 || i == map_height - 1 || j == 0 || j == map_width - 1 ) {
-					curr = 1;
+	public Tilemap(String fileName , int size) {
+		
+		mapCode =  new int[size][size];
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			
+//			int mapWidth = Integer.parseInt(br.readLine());
+			
+			String separator = " ";
+			
+			for(int i = 0;i < size;i++) {
+				String line = br.readLine();
+				if(line != null) {
+					String[] token = line.split(separator);
+					for(int j = 0;j < size;j++) {
+						mapCode[j][i] = Integer.parseInt(token[j]);
+					}
 				}
-				System.out.print(curr + " ");
-				if(curr == 1) {
-					g.setColor(Color.BLACK);
-					g.fillRect(x + i*tile_size, y + j*tile_size, tile_size	, tile_size);
-				}else if(curr == 0) {
-					g.setColor(Color.GREEN);
-					g.drawRect(x + i*tile_size, y + j*tile_size, tile_size, tile_size);
-				}
-				System.out.println();
 			}
+			br.close();
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
+	}
+	
+	public Tilemap(String fileName) {
+		
+		mapCode = new int[30][30];
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			
+			String separator = " ";
+			
+			for(int i = 0;i < 30;i++) {
+				
+				String line = br.readLine();
+				if(line != null) {
+					String[] token = line.split(separator);
+					for(int j = 0;j < 30;j++) {
+						mapCode[j][i] = Integer.parseInt(token[j]);
+					}
+				}
+			}
+			br.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int[][] getMapCode() {
+		return mapCode;
+	}
+
+	public void setMapCode(int[][] mapCode) {
+		this.mapCode = mapCode;
 	}
 }
